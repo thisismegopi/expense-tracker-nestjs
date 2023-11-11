ab09eb30e184f37883436aa9a97cd9506bd503a9
Gopi <gopinath10007@gmail.com>
2023-11-11 13:37:26 +0530

    update error schema

--- src/common/common.dto.ts
+++ src/common/common.dto.ts
@@ -2,11 +2,11 @@ import { HttpStatus } from '@nestjs/common';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class DefaultException {
-    @ApiProperty({ enum: HttpStatus })
+    @ApiProperty({ type: 'string', enum: HttpStatus })
     statusCode: HttpStatus;
 
-    @ApiProperty()
-    message: any;
+    @ApiProperty({ oneOf: [{ type: 'string' }, { type: 'array', items: { type: 'string' } }] })
+    message: string;
 
     @ApiProperty()
     error: string;
